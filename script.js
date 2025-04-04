3dd9cff95f1b14ce7380e153a9f3e241a4689a1f
adminmonikagautam <amitsharma.webi@gmail.com>
2025-04-05 02:20:19 +0400

    script.js

--- script.js
+++ script.js
@@ -1,35 +1,79 @@
-const API_KEY = "19e3ac4e";
-const searchInput = document.getElementById("searchInput");
-const moviesContainer = document.getElementById("moviesContainer");
-const trendingContainer = document.getElementById("trendingMovies");
+const apiKey = "71a0cb256ce6112edd9d3fd192bab592";
+let page = 1;
+let currentGenre = "";
 
-// Search movies
-searchInput.addEventListener("keyup", async () => {
-  const query = searchInput.value;
-  if (query.length < 3) return;
+document.getElementById("loadMore").addEventListener("click", () => {
+  page++;
+  fetchMovies();
+});
 
-  const res = await fetch(`https://www.omdbapi.com/?s=${query}&apikey=${API_KEY}`);
-  const data = await res.json();
-
-  if (data.Search) {
-    moviesContainer.innerHTML = data.Search.map(movie => `
-      <div class="movie" onclick="getMovieDetails('${movie.imdbID}')">
-        <img src="${movie.Poster}" alt="${movie.Title}" />
-        <h3>${movie.Title}</h3>
-      </div>
-    `).join("");
+document.getElementById("search").addEventListener("input", (e) => {
+  const query = e.target.value;
+  page = 1;
+  if (query) {
+    fetchSearchResults(query);
+  } else {
+    fetchMovies();
   }
 });
 
-// Trending movies
-const trendingIDs = ["tt0111161", "tt0068646", "tt0468569", "tt0109830"];
-trendingContainer.innerHTML = trendingIDs.map(id => `
-  <div class="movie" onclick="getMovieDetails('${id}')">
-    <img src="https://img.omdbapi.com/?i=${id}&apikey=${API_KEY}" />
-    <h3>ID: ${id}</h3>
-  </div>
-`).join("");
+function fetchMovies() {
+  let url = `https://api.themoviedb.org/3/trending/movie/week?api_key=${apiKey}&page=${page}`;
+  if (currentGenre) {
+    url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=${currentGenre}&page=${page}`;
+  }
 
-function getMovieDetails(id) {
-  window.location.href = `movie.html?id=${id}`;
+  fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      displayMovies(data.results);
+    });
 }
+
+function fetchSearchResults(query) {
+  fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${query}`)
+    .then(res => res.json())
+    .then(data => {
+      document.getElementById("movieList").innerHTML = "";
+      displayMovies(data.results);
+    });
+}
+
+function displayMovies(movies) {
+  const list = document.getElementById("movieList");
+  movies.forEach(movie => {
+    const div = document.createElement("div");
+    div.className = "movie";
+    div.innerHTML = `
+      <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />
+      <h3>${movie.title}</h3>
+    `;
+    div.onclick = () => {
+      localStorage.setItem("movieId", movie.id);
+      window.location.href = "movie.html";
+    };
+    list.appendChild(div);
+  });
+}
+
+function fetchGenres() {
+  fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}`)
+    .then(res => res.json())
+    .then(data => {
+      const genreDiv = document.getElementById("genreFilter");
+      data.genres.forEach(genre => {
+        const btn = document.createElement("button");
+        btn.innerText = genre.name;
+        btn.onclick = () => {
+          currentGenre = genre.id;
+          page = 1;
+          document.getElementById("movieList").innerHTML = "";
+          fetchMovies();
+        };
+        genreDiv.appendChild(btn);
+      });
+    });
+}
+
+fetchGenres();
+fetchMovies();
