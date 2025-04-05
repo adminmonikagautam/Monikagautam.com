8383c7861da419cdc88b40e344e25576bacc505e
adminmonikagautam <amitsharma.webi@gmail.com>
2025-04-05 09:10:32 +0400

    firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,8 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Import Firebase modules
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+// Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyB8q0-dKDSY9oxhEfnb4Nij30VZSFfNa34",
   authDomain: "monikagautam-cfbc4.firebaseapp.com",
@@ -6,5 +10,11 @@ const firebaseConfig = {
   storageBucket: "monikagautam-cfbc4.firebasestorage.app",
   messagingSenderId: "225196060820",
   appId: "1:225196060820:web:7d4002d2a4bcdf46c20481",
-  measurementId: "G-4KYML15KJK"
+  measurementId: "G-4KYML15KJK",
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export { app, analytics };
