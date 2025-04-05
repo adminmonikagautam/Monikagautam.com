5e25691fbd1496a0b96e2d1f60a4bd0422b4be27
adminmonikagautam <amitsharma.webi@gmail.com>
2025-04-05 11:51:22 +0400

    Update and rename firebase-config.js to firebase.js

---
+++ firebase.js
@@ -0,0 +1,12 @@
+// Firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyB8q0-dKDSY9oxhEfnb4Nij30VZSFfNa34",
+  authDomain: "monikagautam-cfbc4.firebaseapp.com",
+  projectId: "monikagautam-cfbc4",
+  storageBucket: "monikagautam-cfbc4.appspot.com",
+  messagingSenderId: "225196060820",
+  appId: "1:225196060820:web:7d4002d2a4bcdf46c20481",
+  measurementId: "G-4KYML15KJK"
+};
+
+firebase.initializeApp(firebaseConfig);
