cdffe2c0d26e56580435623f360b43dcc4cddaf6
adminmonikagautam <amitsharma.webi@gmail.com>
2025-04-05 11:55:20 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 const tmdbKey = "7e14501d8b8b298c17837054b1eb2f7c";
-const omdbKey = "e3431b73";
-
 const movieSection = document.getElementById("movieSection");
 
 window.onload = () => {
@@ -11,13 +9,23 @@ function fetchPopularMovies() {
   fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${tmdbKey}`)
     .then(res => res.json())
     .then(data => {
-      displayMovies(data.results);
+      if (data.results) {
+        displayMovies(data.results);
+      } else {
+        movieSection.innerHTML = "<p>No movies found.</p>";
+      }
+    })
+    .catch(err => {
+      console.error("Error fetching popular movies:", err);
+      movieSection.innerHTML = "<p>Error loading movies.</p>";
     });
 }
 
 function displayMovies(movies) {
   movieSection.innerHTML = "";
   movies.forEach(movie => {
+    if (!movie.poster_path) return; // Skip if no poster
+
     const div = document.createElement("div");
     div.classList.add("movieCard");
     div.innerHTML = `
@@ -33,9 +41,19 @@ function searchMovies() {
   const query = document.getElementById("searchInput").value;
   fetch(`https://api.themoviedb.org/3/search/movie?api_key=${tmdbKey}&query=${query}`)
     .then(res => res.json())
-    .then(data => displayMovies(data.results));
+    .then(data => {
+      if (data.results) {
+        displayMovies(data.results);
+      } else {
+        movieSection.innerHTML = "<p>No search results.</p>";
+      }
+    })
+    .catch(err => {
+      console.error("Error searching:", err);
+      movieSection.innerHTML = "<p>Search error.</p>";
+    });
 }
 
-function showMovieDetails(movieId) {
-  window.location.href = `movie.html?id=${movieId}`;
+function showMovieDetails(id) {
+  window.location.href = `movie.html?id=${id}`;
 }
