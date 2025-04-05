1b2b941aa6323c4e845010309e19faf9607141ef
adminmonikagautam <amitsharma.webi@gmail.com>
2025-04-05 10:46:46 +0400

    firebase.js

--- firebase.js
+++ firebase.js
@@ -1,20 +1,26 @@
-// Import Firebase modules
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
 
-// Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyB8q0-dKDSY9oxhEfnb4Nij30VZSFfNa34",
   authDomain: "monikagautam-cfbc4.firebaseapp.com",
   projectId: "monikagautam-cfbc4",
-  storageBucket: "monikagautam-cfbc4.firebasestorage.app",
+  storageBucket: "monikagautam-cfbc4.appspot.com",
   messagingSenderId: "225196060820",
   appId: "1:225196060820:web:7d4002d2a4bcdf46c20481",
-  measurementId: "G-4KYML15KJK",
+  measurementId: "G-4KYML15KJK"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const auth = getAuth();
+const provider = new GoogleAuthProvider();
 
-export { app, analytics };
+document.getElementById("signInBtn").addEventListener("click", () => {
+  signInWithPopup(auth, provider)
+    .then(result => {
+      alert(`Welcome ${result.user.displayName}`);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+});
