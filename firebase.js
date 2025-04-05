337c92f7007d5303ea4db4fd2a7f77e4e5b309ab
adminmonikagautam <amitsharma.webi@gmail.com>
2025-04-05 09:05:12 +0400

    firebase.js

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB8q0-dKDSY9oxhEfnb4Nij30VZSFfNa34",
+  authDomain: "monikagautam-cfbc4.firebaseapp.com",
+  projectId: "monikagautam-cfbc4",
+  storageBucket: "monikagautam-cfbc4.firebasestorage.app",
+  messagingSenderId: "225196060820",
+  appId: "1:225196060820:web:7d4002d2a4bcdf46c20481",
+  measurementId: "G-4KYML15KJK"
+};
